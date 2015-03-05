4806d0ada5edfc438b4f8c143637d2d553e95013
kigesui <kigesui@hotmail.com>
2015-03-05 18:51:30 -0500

    added functions for Type in helper.c

--- helper.c
+++ helper.c
@@ -1,11 +1,19 @@
 #include "helper.h"
 #include "debug.h"
+#include "rpc.h"
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 
+
+
+/******************************************************************************
+ * myread and mywrite
+ *
+ * 
+ *****************************************************************************/
 // div big chunk into smaller chunks and receive one by one
 ssize_t myread(int fd, char* const buffer, const int buffer_len)
 {
@@ -43,4 +51,41 @@ ssize_t mywrite(int fd, const char* const buffer, const int buffer_len)
     }
     DEBUG("mywrite: sent %d of %d",write_so_far,buffer_len);
     return write_so_far;
+}
+
+
+/******************************************************************************
+ * myread and mywrite
+ *
+ * 
+ *****************************************************************************/
+int compute_type_int(int &type_int, const bool is_input, const bool is_output, 
+                      const char arg_type, const unsigned int arg_size ){
+    type_int = 0;
+    if ( is_input ) type_int = type_int | 1 << ARG_INPUT ;
+    if ( is_output ) type_int = type_int | 1 << ARG_OUTPUT ;
+    type_int = type_int | arg_type << 16 ;
+    if ( ( arg_size & 0xFFFF0000 ) != 0 ) return -1;
+    type_int = type_int | arg_size ;
+    return 0;
+}
+
+int type_is_input(bool &is_input, const unsigned int type){
+    is_input = 0x80000000 & type;
+    return 0;
+}
+
+int type_is_output(bool &is_output, const unsigned int type){
+    is_output = 0x40000000 & type;
+    return 0;
+}
+
+int type_arg_type(char &arg_type, const unsigned int type){
+    arg_type = (type >> 16) & 0x000000FF;
+    return 0;
+}
+
+int type_arg_size(int &arg_size, const unsigned int type){
+    arg_size = type & 0x0000FFFF;
+    return 0;
 }
--- helper.h
+++ helper.h
@@ -13,18 +13,31 @@
  *  returns # of chars read if successful
  *          -1 if fail
  */
-extern ssize_t myread(int fd, char* const buffer, const unsigned int buffer_len);
+ssize_t myread(int fd, char* const buffer, const unsigned int buffer_len);
 
 /** write() that can take a large buffer_len
- *  returns # of chars written if successful
+ *  returns # of chars read if successful
  *          -1 if fail
  */
-extern ssize_t mywrite(int fd, const char* const buffer, const unsigned int buffer_len);
+ssize_t mywrite(int fd, const char* const buffer, const unsigned int buffer_len);
 
 
-
-
-
+/**
+ * returns the type as specified in assignment
+ * 1st byte: XY00 0000  - X: is input, Y: is output
+ * 2nd byte: 0000 0ZZZ  - Z: type
+ * 3rd byte: SSSS SSSS  - S: size
+ * 4th byte: SSSS SSSS  - S=0 if scalar
+ *        
+ * returns 0 if successful
+ *         -1 if fails
+ */
+int compute_type_int(int &type_int, const bool is_input, const bool is_output, 
+                      const unsigned int arg_type, const unsigned int arg_size );
+int type_is_input(bool &is_input, const int type);
+int type_is_output(bool &is_output, const int type);
+int type_arg_type(int &arg_type, const int type);
+int type_arg_size(int &arg_size, const int type);
 
 
 #endif // HELPER_H
