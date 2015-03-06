01eaf8f59e0aa8e3da797ac520e23aa3bdc424bc
kigesui <kigesui@hotmail.com>
2015-03-06 14:44:37 -0500

    fixed: SERVER_ADDRESS --> BINDER_ADDRESS

--- binder.c
+++ binder.c
@@ -15,8 +15,6 @@
 #include <errno.h>
 
 #include <string.h>
-#include <string> // sizeof
-#include <iostream>
 
 #include "debug.h"
 #include "helper.h"
@@ -27,53 +25,61 @@ using namespace std;
 
 int main(int argc, char** argv) {
 
-    int server_fd = 0;
-    struct sockaddr_in serv_addr;
-    unsigned int serv_addr_len = sizeof(serv_addr);
+    int binder_fd = 0;
+    struct sockaddr_in binder_addr;
+    unsigned int binder_addr_len = sizeof(binder_addr);
     struct hostent* host;
 
     // socket()
-    if((server_fd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+    if((binder_fd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
     {
-        cerr << "Error : socket() failed" << endl;
+        fprintf(stderr,"Error : socket() failed\n");
         return 1;
     }
-    memset(&serv_addr, '0', serv_addr_len);
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    memset(&binder_addr, '0', binder_addr_len);
+    binder_addr.sin_family = AF_INET;
+    binder_addr.sin_addr.s_addr = INADDR_ANY;
 
 #ifdef _ENABLE_DEBUG_
-    serv_addr.sin_port = htons(DEBUG_BINDER_PORT);
+    binder_addr.sin_port = htons(DEBUG_BINDER_PORT);
 #else
-    serv_addr.sin_port = 0;
+    binder_addr.sin_port = 0;
 #endif
 
     // bind()
-    if ( bind(server_fd, (const struct sockaddr *)(&serv_addr), serv_addr_len) < 0 ) {
-        cerr << "Error : bind() failed" << endl;
+    if ( bind(binder_fd, (const struct sockaddr *)(&binder_addr), binder_addr_len) < 0 ) {
+        fprintf(stderr,"Error : bind() failed\n");
         return 1;
     }
 
+    // listen()
+    if ( listen(binder_fd,100) < 0 ) {
+        fprintf(stderr,"Error : listen() failed\n");
+        return 1;
+    }
+
+    // prints binder's BINDER_ADDRESS and BINDER_PORT
+
     // call gethostname to get the full host name
-    char hostname[100];
+    char hostname[100] = { 0 };
     if ( gethostname(hostname,sizeof(hostname)) < 0 ) {
-        cerr << "Error : gethostname()" << endl;
+        fprintf(stderr,"Error : gethostname()\n");
         return 1;
     }
     if ( (host = gethostbyname(hostname)) == NULL ) {
-        cerr << "Error : gethostbyname() for " << hostname << endl;
+        fprintf(stderr,"Error : gethostbyname() for %s\n",hostname);
         return 1;
     }
-    // refresh serv_addr to output the port number
-    if ( getsockname(server_fd, (struct sockaddr *)&serv_addr, &serv_addr_len ) < 0 ){
-        cerr << "Error : getsockname() failed" << endl;
+    // refresh binder_addr to output the port number
+    if ( getsockname(binder_fd, (struct sockaddr *)&binder_addr, &binder_addr_len ) < 0 ){
+        fprintf(stderr,"Error : getsockname() failed\n");
         return 1;
     }
 
     unsigned int ipv4;
     unsigned char ipb1, ipb2, ipb3, ipb4;
-    if ( get_ip_from_socket(ipv4,server_fd) < 0 ) {
-        fprintf(stderr,"couldn't get IP address for this server\n");
+    if ( get_ip_from_socket(ipv4,binder_fd) < 0 ) {
+        fprintf(stderr,"Error : couldn't get IP address for this server\n");
         return 1;
     }
     ipb1 = (ipv4 >> 24) & 0xFF;
@@ -81,8 +87,9 @@ int main(int argc, char** argv) {
     ipb3 = (ipv4 >> 8) & 0xFF;
     ipb4 = (ipv4 >> 0) & 0xFF;
 
-    fprintf(stdout,"SERVER_ADDRESS %u.%u.%u.%u\n",ipb1, ipb2, ipb3, ipb4);
-    fprintf(stdout,"SERVER_PORT %d\n",ntohs(serv_addr.sin_port));
+    fprintf(stdout,"BINDER_ADDRESS %u.%u.%u.%u\n",ipb1, ipb2, ipb3, ipb4);
+    fprintf(stdout,"BINDER_PORT %d\n",ntohs(binder_addr.sin_port));
+
 
 
 
