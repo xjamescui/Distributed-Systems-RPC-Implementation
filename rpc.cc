3133b69837b36a8819bf2aa67f09cf2fb2e49a87
kigesui <kigesui@hotmail.com>
2015-03-15 05:18:23 -0400

    implemented the helper functions, 1 todo left ,and read_message  must have char**, also preferable returns a length

--- rpc.cc
+++ rpc.cc
@@ -31,10 +31,10 @@ int connect_server_to_binder();
  *
  */
 int ask_binder_for_host(int binder_fd, unsigned int *ip, unsigned int *port,
-                        unsigned int fct_name_len, char *name,
+                        unsigned int name_len, char *name,
                         unsigned int arg_types_len, int *arg_types);
 int send_execute_to_server(int server_fd,
-                           unsigned int fct_name_len, char *name,
+                           unsigned int name_len, char *name,
                            unsigned int arg_types_len, int *arg_types, void** args);
 
 /**
@@ -70,11 +70,6 @@ int rpcCall(char* name, int* argTypes, void** args)
     char* binder_port_str;
     int binder_port;
 
-    // declare vars for buffer
-    char* w_buffer = NULL;
-    unsigned int w_buffer_len;
-    ssize_t write_len;
-
     // get environment variables
     binder_address = getenv(BINDER_ADDRESS_STRING);
     binder_port_str = getenv(BINDER_PORT_STRING);
@@ -90,6 +85,10 @@ int rpcCall(char* name, int* argTypes, void** args)
         return -1;
     }
 
+    // done with binder, close it
+    close(binder_fd);
+
+    // now query server
     int server_fd;
     unsigned int server_ip;
     unsigned int server_port;
@@ -119,6 +118,7 @@ int rpcCall(char* name, int* argTypes, void** args)
         return opCode;
     }
 
+    close(server_fd);
     return 0;
 }
 
@@ -404,3 +404,152 @@ int extract_registration_results(char *msg, int* result)
 void handle_client_message(char* msg, unsigned int client_fd) {
 
 }
+
+
+
+
+
+
+
+
+/**
+ * rpcCall helper functions
+ *
+ *
+ */
+int ask_binder_for_host(int binder_fd, unsigned int *ip, unsigned int *port,
+                        unsigned int name_len, char *name,
+                        unsigned int arg_types_len, int *arg_types) {
+
+    // for the buffer
+    char* rw_buffer;
+    unsigned int rw_buffer_len;
+    ssize_t read_len, write_len;
+
+    // stuff to be extracted
+    unsigned int msg_len;
+    char msg_type;
+    unsigned server_ip, server_port;
+    int result;
+
+    // assemble
+    assemble_msg(&rw_buffer,&rw_buffer_len,MSG_LOC_REQUEST,
+        name_len,name,arg_types_len,arg_types);
+
+    // send loc request to binder
+    write_len = write_message(binder_fd,rw_buffer,rw_buffer_len);
+    free(rw_buffer);
+    rw_buffer = NULL;
+    if ( write_len < rw_buffer_len ) {
+        fprintf(stderr, "Error : couldn't send loc request\n");
+        return -1;
+    }
+
+    // wait for answer
+    read_len = read_message(&rw_buffer,g_binder_fd);
+    if ( read_len < 0 ) {
+        fprintf(stderr, "Error : couldn't read reply of loc request\n");
+        return -1;
+    }
+
+    // extract length and type
+    extract_msg_len_type(&msg_len,&msg_type,rw_buffer);
+    rw_buffer_len = msg_len + 5;
+
+    // check type
+    switch (msg_type) {
+    case MSG_LOC_SUCCESS : {
+        extract_msg(rw_buffer,rw_buffer_len,MSG_LOC_SUCCESS,&server_ip,&server_port);
+        free(rw_buffer);
+        *ip = server_ip;
+        *port = server_port;
+    } break;
+    case MSG_LOC_FAILURE : {
+        extract_msg(rw_buffer,rw_buffer_len,MSG_LOC_FAILURE,&result);
+        free(rw_buffer);
+        return result; // TODO : check if this is really what we want to return
+    } break;
+    default: 
+        fprintf(stderr, "Error : asking for server ip and port, got invalid type %x\n",msg_type);
+        free(rw_buffer);
+        return -1;
+    }
+
+    // success!
+    return 0;
+}
+int send_execute_to_server(int server_fd,
+   unsigned int name_len, char *name,
+   unsigned int arg_types_len, int *arg_types, void** args) {
+
+    // for the buffer
+    char* rw_buffer;
+    unsigned int rw_buffer_len;
+    ssize_t read_len, write_len;
+
+    // for the message
+    unsigned int msg_len;
+    char msg_type;
+    unsigned int reply_name_len; // not sure if these are needed
+    char* reply_name = NULL; // not sure if these are needed
+    unsigned int reply_arg_types_len; // not sure if these are needed
+    int* reply_arg_types = NULL; // not sure if these are needed
+    void** reply_args = NULL;
+    int result;
+
+    // assemble message
+    assemble_msg(&rw_buffer,&rw_buffer_len,MSG_EXECUTE,
+                 name_len,name,arg_types_len,arg_types,args);
+
+    // send to server
+    write_len = write_message(server_fd,rw_buffer,rw_buffer_len);
+    free(rw_buffer);
+    rw_buffer = NULL;
+    if ( write_len < rw_buffer_len) {
+        fprintf(stderr, "Error : couldn't send execute\n");
+        return -1;
+    }
+
+    // wait for answer
+    read_len = read_message(&rw_buffer,server_fd);
+    if ( read_len < 0 ) {
+        fprintf(stderr, "Error : couldn't read reply of loc request\n");
+        return -1;
+    }
+
+    // extract len and type
+    extract_msg_len_type(&msg_len,&msg_type,rw_buffer);
+    rw_buffer_len = msg_len + 5;
+
+    switch ( msg_type ) {
+    case MSG_EXECUTE_SUCCESS: {
+        extract_msg(rw_buffer,rw_buffer_len,MSG_EXECUTE_SUCCESS,
+            &reply_name_len,&reply_name,&reply_arg_types_len,&reply_arg_types,&reply_args);
+        free(rw_buffer);
+    } break;    
+    case MSG_EXECUTE_FAILURE: {
+        extract_msg(rw_buffer,rw_buffer_len,MSG_EXECUTE_FAILURE,&result);
+        free(rw_buffer);
+        return result;
+    } break;
+    default: 
+        free(rw_buffer);
+        fprintf(stderr, "Error : asking for server execute, got invalid type %x\n",msg_type);
+        return -1;
+    }
+
+
+    // copy args over
+    // TODO: iterate through
+    DEBUG("not setting answer to **args yet");
+
+    // TODO: Free everything
+
+    return 0;
+}
+
+
+
+
+
+
