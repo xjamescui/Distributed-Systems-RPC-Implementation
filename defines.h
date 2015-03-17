74e70bfdd3fb983c8b8aba2d06294abf14e42516
kigesui <kigesui@hotmail.com>
2015-03-17 15:38:29 -0400

    added new defines for cache requests messages

--- defines.h
+++ defines.h
@@ -33,6 +33,10 @@
 #define MSG_EXECUTE_SUCCESS     0x31
 #define MSG_EXECUTE_FAILURE     0x32
 
+#define MSG_LOC_CACHE_REQUEST   0x40
+#define MSG_LOC_CACHE_SUCCESS   0x41
+#define MSG_LOC_CACHE_FAILURE   0x42
+
 #define MSG_TERMINATE           0x70
 
 #define MAX_RW_CHUNK_SIZE       16348
